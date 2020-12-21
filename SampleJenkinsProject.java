245f2f5d514c58dc674d36533d383aeee49a5924
Ayush Chaturvedi <ayushchaturvedi220@gmail.com>
2020-12-21 12:29:18 +0530

    commit for JenkinOn21Dec20

---
+++ SampleJenkinsProject.java
@@ -0,0 +1,14 @@
+public class SampleJenkinsProject
+{
+public static void main(String[] args)
+{
+SampleJenkinsProject sjp=new SampleJenkinsProject();
+String result=sjp.getData();
+System.out.println("The result is "+rresult);
+System.out.println("Welcome to Jenkins");
+}
+public String getData()
+{
+return "Hello world from getData";
+}
+}
